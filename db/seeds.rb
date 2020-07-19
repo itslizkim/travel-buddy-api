4d45bf3e188648bb50cc500e527a8f3e4218b399
itslizkim <elizabeth.kim93@gmail.com>
2020-07-19 00:21:20 -0400

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 User.destroy_all
 Plan.destroy_all
 Destination.destroy_all
@@ -17,12 +11,11 @@ activities = JSON.parse(res_body)
 
 activities["results"].each do |activity|
   Activity.create(
-    title: activity["title"], 
-    vote_average: activity["vote_average"], 
-    vote_count: activity["vote_count"],
-    overview: activity["overview"], 
+    name: activity["name"], 
+    location: activity["location"], 
+    rating: activity["rating"],
+    description: activity["description"], 
     image_url: "https://image.tmdb.org/t/p/w500/#{activity['poster_path']}", 
-    release_year: activity["release_date"].split("-")[0].to_i
   )
 end 
 
